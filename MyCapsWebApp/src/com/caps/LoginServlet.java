4636fadf7693c30be42958a9e291d4ece92d5f87
Vaish0205 <42334239+Vaish0205@users.noreply.github.com>
2018-08-17 22:28:05 +0530

    Add files via upload

---
+++ MyCapsWebApp/src/com/caps/LoginServlet.java
@@ -0,0 +1,21 @@
+package com.caps;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet{
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String regno = req.getParameter("regno");
+		String pswd = req.getParameter("passwd");
+		
+		System.out.println(regno);
+		System.out.println(pswd);
+	}
+}
